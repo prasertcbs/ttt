87a306ef50b3660a831e2f6dcec7cd1912d39e4f
Prasert Kanawattanachai <bluecotton.k@gmail.com>
2021-06-05 21:13:00 +0700

    fix server

--- server.js
+++ server.js
@@ -32,9 +32,13 @@ app.addHook('onClose', async (instance, done) => {
 });
 
 // Start listening.
-app.listen(process.env.PORT || 3000, (err) => {
-  if (err) {
-    app.log.error(err);
-    process.exit(1);
-  }
+app.listen(process.env.PORT, '0.0.0.0', (err, address) => {
+  if (err) throw err;
+  app.log.info(`server listening on ${address}`);
 });
+// app.listen(process.env.PORT || 3000, (err) => {
+//   if (err) {
+//     app.log.error(err);
+//     process.exit(1);
+//   }
+// });
